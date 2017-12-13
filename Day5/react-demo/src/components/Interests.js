2e31ffc63d6b4f68a72f16d55b57b092918040a4
Ayush Rawal <31813648+Oguz-Khagan@users.noreply.github.com>
2017-12-13 23:42:11 +0530

    Day 6 over

--- Day5/react-demo/src/components/Interests.js
+++ Day5/react-demo/src/components/Interests.js
@@ -2,11 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Interest extends Component {
+
+    constructor(props){
+        this.state = {
+            interests:props.interests
+            
+        }
+    }
+
     render() {
             return ( < div className = "App" >
-                    <h1 > Interests of { this.props.name }: </h1>  
-                    <ul>{
-                       this.props.interests.map(name => <li>{name}</li>)}   
+            <div className ="row">
+                <div className="col-md-6">
+                    <input type = "text" placeholder="Enter interest here" className="input-lg" />
+                </div>  
+                <div className="col-md-3">
+                <button type="submit" className="btn btn-success" >Submit</button>
+                </div>  
+            </div>  
+                    <h1 > Interests of { this.props.name }: </h1>
+                    <ul className="list-group" >{
+                       this.props.interests.map(name => <li key="name" className="list-group-item" >{name}</li>)} 
+                       {/* key can also be index(more preferable) */}
                         </ul>					
                         </div>);
                         }
